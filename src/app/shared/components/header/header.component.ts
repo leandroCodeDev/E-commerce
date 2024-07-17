8793c9c519d36865d9f44e8312fcdc4c01749d40
Michel Vieira <michel@darochavieira.com>
2024-07-17 19:06:35 -0300

    estilização do header com PrimeNG

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import {Router, RouterLink} from "@angular/router";
 import {FormsModule} from "@angular/forms";
+import {Button, ButtonDirective} from "primeng/button";
+import {BadgeModule} from "primeng/badge";
+import {InputTextModule} from "primeng/inputtext";
+import {InputGroupModule} from "primeng/inputgroup";
+import {PanelModule} from "primeng/panel";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     RouterLink,
-    FormsModule
+    FormsModule,
+    Button,
+    BadgeModule,
+    InputTextModule,
+    InputGroupModule,
+    ButtonDirective,
+    PanelModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
@@ -17,6 +28,13 @@ export class HeaderComponent {
   numItemsCarrinho = 1;
 
   constructor(private router: Router) {}
+
+  get badgeValue() {
+    if (this.numItemsCarrinho) {
+      return this.numItemsCarrinho.toString()
+    }
+    return '';
+  }
 
   onSubmit() {
     if (this.valorPesquisa) {
