6bf6be81956d89a2f8af38c9772d82f8b2276290
Michel Vieira <michel@darochavieira.com>
2024-07-16 23:30:55 -0300

    header seaparado da home e adicionado query de pesquisa

--- src/app/pages/listagem-produtos/listagem-produtos.component.ts
+++ src/app/pages/listagem-produtos/listagem-produtos.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-listagem-produtos',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './listagem-produtos.component.html',
   styleUrl: './listagem-produtos.component.scss'
 })
-export class ListagemProdutosComponent {
+export class ListagemProdutosComponent implements OnInit {
+  valorPesquisa = '';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(
+      params => {
+        this.valorPesquisa = params['q'] || '';
+      }
+    );
+  }
 
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    FormsModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  valorPesquisa = '';
 
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    if (this.valorPesquisa) {
+      this.router.navigate(
+        ['/lista-produtos'],
+        { queryParams: { q: this.valorPesquisa } }
+      );
+      let route = '/lista-produtos';
+    }
+  }
 }
