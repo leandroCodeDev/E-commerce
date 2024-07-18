99e77336822a495cc82077e4bea5ef3b0ce720f5
Suene Souza <suene.vs@gmail.com>
2024-07-18 18:50:10 -0300

    Criação do banner/carousel)

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [PanelModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
