d1b928696fc0e84967630e52a79e34fe0b4bfd02
Suene Souza <suene.vs@gmail.com>
2024-07-22 22:26:46 -0300

    Atualização detalhes do produto

--- src/app/shared/services/comentarios/comentarios.service.ts
+++ src/app/shared/services/comentarios/comentarios.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Comentario } from '../../interfaces/comentario/comentario';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,10 @@ export class ComentariosService {
   constructor(private httpClient: HttpClient) {}
 
   getTodosComentarios() {
-    return this.httpClient.get(`http://localhost:3000/comentarios`)
+    return this.httpClient.get<Array<Comentario>>('http://localhost:3000/comentarios')
   }
 
   getComentarioProdutoId(produtoId: string) {
-    return this.httpClient.get(`http://localhost:3000/comentarios?produto_id=${produtoId}`)
+    return this.httpClient.get<Array<Comentario>>(`http://localhost:3000/comentarios?produto_id=${produtoId}`)
   }
 }
